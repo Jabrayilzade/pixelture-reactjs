cb01721179168c9b2e8914cb4d291cb135d662a9
Jabrayilzade <cnixad@gmail.com>
2021-07-19 14:55:47 +0400

    New section added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,15 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg'
 import sass from '../sass/header.module.sass'
-import {makeStyles} from "@material-ui/core"
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const useStyles = makeStyles(() => ({
-    btnDemo: {
-        width: 200,
-        height: 50,
-        borderRadius: 45,
-        border: '1px #fc687b',
-        transition: 'color .2s, box-shadow .2s, background-color .2s',
-
-        '&:hover': {
-            background: ' $colorBaseBrandRed',
-            boxShadow: '0 0 0 6px rgb(253 87 108 / 25%)',
-        }
-    }
-}))
-
 export const Header = () => {
-    const muiStyles = useStyles()
-
     return (
         <div className={sass.root}>
             <div className={sass.header}>
                 <div className={sass.header__logo}>
                     <img className={sass.header__img} src={logo} alt=''/>
-                    <a className={sass.header__title} href="https://github.com/Jabrayilzade" target="_blank">
+                    <a className={sass.header__title} href="https://github.com/Jabrayilzade" target="_blank" rel="noreferrer">
                         pixeltrue
                     </a>
                 </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
 import sass from '../sass/home-page.module.sass'
 import showBtnImg from '../assets/images/show-btn.svg'
@@ -9,7 +10,10 @@ import bridgeProfits from '../assets/images/bridge-profits.svg'
 import inquivix from '../assets/images/inguivix.svg'
 import manyRequests from '../assets/images/many-requests.svg'
 import buildForMars from '../assets/images/build-for-mars.svg'
-
+import poorQuality from '../assets/images/poor-quality.svg'
+import badCommunication from '../assets/images/bad-communication.svg'
+import missedDeadlines from '../assets/images/missed-deadlines.svg'
+import {Grow} from "@material-ui/core";
 
 const SPONSORS = [
     {
@@ -50,7 +54,40 @@ const SPONSORS = [
     },
 ]
 
+const WORK_CARDS = [
+    {
+        id: 1,
+        img: poorQuality,
+        title: 'Poor Quality',
+        text: 'You’re tired of the guessing game to find the right designer.'
+    },
+    {
+        id: 2,
+        img: badCommunication,
+        title: 'Bad Communication',
+        text: 'You’re fed up with having to explain yourself multiple times.'
+    },
+    {
+        id: 3,
+        img: missedDeadlines,
+        title: 'Missed deadlines',
+        text: 'You lose hope as deadlines are missed over and over again.'
+    }
+]
+
 export const HomePage = () => {
+
+    const [infoAnimations, setInfoAnimations] = React.useState({
+        isH1Shown: false, isH2Shown: false, isImgShown: false
+    })
+
+    React.useEffect(() => {
+        setInfoAnimations({isH1Shown: true, isH2Shown: true, isImgShown: true})
+
+        return () => setInfoAnimations(null)
+    }, [])
+
+
     return (
         <>
             <Header/>
@@ -58,20 +95,26 @@ export const HomePage = () => {
                 <div className={sass.superhero}>
                     <div className={sass.superhero__container}>
                         <div className={sass.superhero__info}>
-                            <h1 className={sass.superhero__design}>
-                                <strong>Get A Superhero Design Team<br/>To Grow Your Business</strong>
-                            </h1>
-                            <h2 className={sass.superhero__provide}>
-                                We provide an experienced design team that works with you everyday to<br/>create
-                                high-quality
-                                design assets to help grow your business. Zero<br/>headaches, fixed pricing and no
-                                contracts.
-                            </h2>
-                            <img
-                                className={sass.superhero__btn}
-                                src={showBtnImg}
-                                alt="https://www.youtube.com/watch?v=A6WEAuSvcgY"
-                            />
+                            <Grow in={infoAnimations.isH1Shown} timeout={{enter: 1000, exit: 1000}}>
+                                <h1 className={sass.superhero__design}>
+                                    <strong>Get A Superhero Design Team<br/>To Grow Your Business</strong>
+                                </h1>
+                            </Grow>
+                            <Grow in={infoAnimations.isH2Shown} timeout={{enter: 1500, exit: 1500}}>
+                                <h2 className={sass.superhero__provide}>
+                                    We provide an experienced design team that works with you everyday to<br/>create
+                                    high-quality
+                                    design assets to help grow your business. Zero<br/>headaches, fixed pricing and no
+                                    contracts.
+                                </h2>
+                            </Grow>
+                            <Grow in={infoAnimations.isImgShown} timeout={{enter: 2000, exit: 2000}}>
+                                <img
+                                    className={sass.superhero__btn}
+                                    src={showBtnImg}
+                                    alt="https://www.youtube.com/watch?v=A6WEAuSvcgY"
+                                />
+                            </Grow>
                         </div>
                     </div>
                 </div>
@@ -86,6 +129,25 @@ export const HomePage = () => {
                                 target="_blank"
                                 style={{backgroundImage: `url(${sponsor.logo})`}}
                             />
+                        ))}
+                    </div>
+                </div>
+                <div className={sass.workCards}>
+                    <h1 className={sass.workCards__title}>We get it. Reliable
+                        designers<br/><span>are hard to find</span></h1>
+                    <div className={sass.workCards__container}>
+                        {WORK_CARDS?.map(card => (
+                            <div key={card.id} className={sass.workCards__card}>
+                                <img className={sass.workCards__img} src={card.img} alt=""/>
+                                <div className={sass.workCards__content}>
+                                    <h3 className={sass.workCards__label}>
+                                        {card.title}
+                                    </h3>
+                                    <p className={sass.workCards__text}>
+                                        {card.text}
+                                    </p>
+                                </div>
+                            </div>
                         ))}
                     </div>
                 </div>
